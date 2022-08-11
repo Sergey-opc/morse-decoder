4ab5ede857977d59821081b7491dbe2c19d696b0
Volodkevich Sergey <volodkevich777@gmail.com>
2022-08-11 16:03:03 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -37,8 +37,35 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const BIN_TABLE = {
+    '10': '.',
+    '11': '-',
+}
+
 function decode(expr) {
-    // write your solution here
+    let string = expr;
+    let pass = '';
+    let pars = '';
+    let convert = '';
+    let word = '';
+    for(let i = 0; i < (expr.length/10); i++){
+        pars = string.slice(0, 10);
+        if(pars[4] === '*'){
+            pass = pass + (word + ' ');
+            convert = '';
+            word = '';
+        }else{
+        for (let j = 9; j > 0; j=j-2){
+            if(pars[j-1] != 0){
+            convert = (BIN_TABLE[pars[j-1] + pars[j]]) + convert;
+            }
+        }
+        word = word + MORSE_TABLE[convert];
+        convert = '';
+        }
+        string = string.slice(10)
+    }
+return(pass + word);
 }
 
 module.exports = {
